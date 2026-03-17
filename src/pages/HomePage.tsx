1f7fa7f4c50a4bde90224eb0c95db25172c89d20
damianmiskow <111329721+damianmiskow@users.noreply.github.com>
2026-03-16 20:46:32 -0400

    amplifytest 1

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,17 +1,16 @@
-import "./HomePage.css"
-import { Header } from '../components/Header'
+import "./HomePage.css";
+import { Header } from "../components/Header";
 
-export function HomePage () {
-    return (
-        <>
-        <title>DM Home Page</title>
-        <Header/>
-        <div className="home-page">
-            <div className="welcome-message">Damian Miskow</div>
-            <div className="welcome-message">Software Development</div>
-        </div>
-        
-        </>
-    )
-
+export function HomePage() {
+  return (
+    <>
+      <title>DM Home Page</title>
+      <Header />
+      <div className="home-page">
+        <div className="welcome-message">Damian Miskow</div>
+        <div className="welcome-message">Software Development</div>
+        <div className="welcome-message">TEST</div>
+      </div>
+    </>
+  );
 }
